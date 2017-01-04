0c7259adebc857a8f15257f2acdfa5898842bca8
Supasate Choochaisri <supasate.c@gmail.com>
2017-01-04 15:29:31 +0700

    List items

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './reducers'
 import { Header } from './components/common'
+import LibraryList from './components/LibraryList'
 
 const App = () => (
   <Provider store={createStore(rootReducer)}>
-    <View>
+    <View style={{ flex: 1 }}>
       <Header headerText="Tech Stack" />
+      <LibraryList />
     </View>
   </Provider>
 )
