02f2e15c95a98fe276cc8f16e224aafea5accc16
Supasate Choochaisri <supasate.c@gmail.com>
2017-01-04 16:17:30 +0700

    Enable spring animation on android

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,12 +4,21 @@ import {
   TouchableWithoutFeedback,
   View,
   LayoutAnimation,
+  UIManager,
+  Platform,
 } from 'react-native'
 import { connect } from 'react-redux'
 import { CardSection } from './common'
 import * as actions from '../actions'
 
 class ListItem extends Component {
+  constructor() {
+    super()
+
+    if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
+      UIManager.setLayoutAnimationEnabledExperimental(true)
+    }
+  }
   componentWillUpdate() {
     LayoutAnimation.spring()
   }
